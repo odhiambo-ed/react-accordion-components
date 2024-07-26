8771cbcfefcc8d2be5f5287eae22ca9fe6dde9f4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-27 00:35:48 +0300

    add flex to app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="mt-4">
+    <div className="d-flex justify-content-center align-items-center mt-4">
       <Accordion />
     </div>
   );
