9e5b5d0bb6ab4dd6793a7e786b8e47f2ec664ba2
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-26 23:19:06 +0300

    map faqs

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -3,7 +3,18 @@ import faqData from '../assets/data'
 
 function Accordion() {
   return (
-    <div>Jesus is still doing it</div>
+    <div className="accordion">
+      {faqData.map((item, index) => 
+        <div className="faq">
+          <div className="title">
+            <h4>{ item.question }</h4>
+          </div>
+          <div className="content">
+            <p>{ item.answer }</p>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
