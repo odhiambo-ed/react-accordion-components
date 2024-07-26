de638f0376a8a00b31c11274090c462acfcf8ade
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-26 23:41:20 +0300

    add handle active index function

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import faqData from '../assets/data'
 import './Accordion.css'
 
 function Accordion() {
+  const [activeIndex, setActiveIndex] = useState(false)
+
+  const handleActiveIndex = (index) => {
+    if (activeIndex === index) {
+      setActiveIndex(false)
+    } else {
+      setActiveIndex(index)
+    }
+  }
   return (
     <div className="accordion d-flex flex-column justify-content-center align-item-center">
-      {faqData.map((item, index) => 
-        <div className="faq">
+      {faqData.map((item, index) => (
+        <div key={index} onClick={handleActiveIndex} className="faq">
           <div className="title">
             <h4>{item.question}</h4>
             <div className="fa-solid fa-plus"></div>
           </div>
           <div className="content">
-            <p>{ item.answer }</p>
+            <p>{item.answer}</p>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
 export default Accordion
